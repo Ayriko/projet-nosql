d08cb446831e2db2a9ad8765df2e05a5a730789d
AMOISKA <aymeric.moiska@ynov.com>
2024-03-04 14:59:14 +0100

    feat: add router & login + register pages

---
+++ link/src/pages/Login.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import LoginComponent from "../components/SignIn";
+
+function Login(): React.JSX.Element {
+    return (
+        <LoginComponent />
+    );
+}
+
+export default Login;
